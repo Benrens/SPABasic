747a823b031936683c9e5437e753275189685d48
Benrens <benrens1@msn.com>
2015-01-23 11:32:02 +0100

    cleanup

--- app.js
+++
@@ -1,8 +0,0 @@
-var App = angular.module('App', []);
-
-App.controller('WebCtrl', function($scope, $http) {
-    $http.get('web.json')
-        .then(function(res){
-            $scope.todos = res.data;
-        });
-});
