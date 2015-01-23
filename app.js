f35456c23c5468c645452487903c4b298c4d3b02
Benrens <benrens1@msn.com>
2015-01-23 14:35:01 +0100

    menu met articles

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-var App = angular.module('App', []);
+    var App = angular.module('App', []);
+    App.controller('WebCtrl', function ($scope, $http) {
 
-App.controller('WebCtrl', function($scope, $http) {
-    $http.get('web.json')
-        .then(function(res){
-            $scope.todos = res.data;
-        });
-});
+        $http.get('web.json')
+            .then(function (res) {
+                $scope.start = res.data;
+            });
+
+        $http.get('/')
+            .then(function(){
+
+                $scope.menu = function (id) {
+                    $scope.articles = $scope.start.slice(id, id + 1);
+                }
+            });
+
+    });
