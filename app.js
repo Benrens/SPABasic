4996bc8cf4962c740dadefdf2964b3f145093e60
Els <edenengelse@gmail.com>
2015-01-23 15:01:41 +0100

    commit

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-    var App = angular.module('App', []);
-    App.controller('WebCtrl', function ($scope, $http) {
+var App = angular.module('App', []);
 
-        $http.get('web.json')
-            .then(function (res) {
-                $scope.start = res.data;
-            });
+App.controller('WebCtrl', function ($scope, $http) {
 
-        $http.get('/')
-            .then(function(){
+    $http.get('web.json')
+        .then(function (res) {
+            $scope.start = res.data;
+        });
 
-                $scope.menu = function (id) {
-                    $scope.articles = $scope.start.slice(id, id + 1);
-                }
-            });
+    $http.get('/')
+        .then(function(){
 
-    });
+            $scope.menu = function (id) {
+                $scope.articles = $scope.start.slice(id, id + 1);
+            }
+        });
+
+});
